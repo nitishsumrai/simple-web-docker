ea33f657ffa111f9ca1c5c60ab102bfb811d6d3e
Nitish Yadav <nitish.yadav@mosaicwellness.in>
2021-03-31 21:12:24 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+  return  res.send('hi pop');
+});
+
+app.listen(3000,(er)=>{
+    if(er){
+    return console.log('error in running server',er);
+    }
+    console.log('server is up and running on port : 3000');
+});
